4cee41556c64f217349de5bec7a00e754127166f
Alex Cheng <alexcheng@Alexs-MacBook-Pro.local>
2016-06-27 16:31:09 -0700

    just some stuff I tried to route

--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,45 @@
-var express = require('express');
-var router = express.Router();
+"use strict"
+const express = require('express');
+const pg = require('pg');
+const bodyParser = require('body-parser');
+const knex = require('../db/knex');
+
+const router = express.Router();
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('category landing page');
+
+router.get('/', (req, res, next) => {
+  knex('categories').select()
+  .then( (categories) => {
+    res.render('category', {categories: category});
+  });
 });
+
+router.get('/:id', (req, res, next) => {
+  console.log(req.params.id); //figure out said ID
+  knex('categories').select().where('id', req.params.id)
+  .then( (categories) => {
+    res.render('category', {categories: category});
+  });
+});
+
+
+router.get('/createNew', function(req, res, next) {
+  res.send('new category landing page');
+});
+
+
+router.post('/search', (req, res, next) => {
+  var categoryName = req.body['category_name'];
+  // console.log(bookTitle);
+  knex.raw('select id, category_name from categories where category_name ~* ?', [`.*${categoryName}*.`])
+  .then( (books) => {
+    var matchingCategory = categories.rows;
+    console.log(matchingCategory);
+    res.render('bookSearch', {category: matchingCategory});
+  });
+});
+
 
 module.exports = router;
