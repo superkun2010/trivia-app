28783df6a82d85e5196efd80e38c678cbaf88cc2
superkun2010 <kuhsu@hotmail.com>
2016-06-27 16:29:30 -0700

    added access to question and answers to json file

--- routes/api.js
+++ routes/api.js
@@ -15,16 +15,18 @@ router.get('/category', function (req, res, next) {
 
 router.get('/question', function (req, res, next) {
 	var category = 'world capitals';
-	var numOfQuestions = 1;
+	var numOfQuestions = 2;
 	// var category = req.body.category;
 	// var numOfQuestions = req.body.numOfQuestions;
 	knex('questions').join('category_questions', 'questions.id', 'category_questions.question_id')
 		.join('categories', 'category_questions.category_id', 'categories.id')
-		.select().orderBy(knex.raw('RANDOM()')).limit(numOfQuestions)
-	.then(function (data) {
-		console.log(data);
-
-		res.json(data);
+		.select().orderBy(knex.raw('RANDOM()')).limit(numOfQuestions).pluck('question_id')
+		.then(function (data) {
+		knex('questions').select().join('answers', 'questions.id', 'answers.question_id').whereIn('question_id', data)
+			.then(function (data) {
+				console.log(data);
+				res.json(data);
+			})
 	})
 })
 
