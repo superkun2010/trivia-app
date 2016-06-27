1f6081346e2e6cb8fac35a0b2ab3980a6eac9121
Alex Cheng <alexcheng@Alexs-MacBook-Pro.local>
2016-06-27 16:34:46 -0700

    commented out search put request

--- routes/category.js
+++ routes/category.js
@@ -30,16 +30,16 @@ router.get('/createNew', function(req, res, next) {
 });
 
 
-router.post('/search', (req, res, next) => {
-  var categoryName = req.body['category_name'];
-  // console.log(bookTitle);
-  knex.raw('select id, category_name from categories where category_name ~* ?', [`.*${categoryName}*.`])
-  .then( (books) => {
-    var matchingCategory = categories.rows;
-    console.log(matchingCategory);
-    res.render('bookSearch', {category: matchingCategory});
-  });
-});
+// router.post('/search', (req, res, next) => {
+//   var categoryName = req.body['category_name'];
+//   console.log(categoryName);
+//   knex.raw('select id, category_name from categories where category_name ~* ?', [`.*${categoryName}*.`])
+//   .then( (books) => {
+//     var matchingCategory = categories.rows;
+//     console.log(matchingCategory);
+//     res.render('bookSearch', {category: matchingCategory});
+//   });
+// });
 
 
 module.exports = router;
