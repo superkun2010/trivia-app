7ed04e2ba5b55b9eb12ee9056b045ff3e0166bd4
superkun2010 <kuhsu@hotmail.com>
2016-06-27 14:04:22 -0700

    pulling question data from postgres

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('APIs');
 });
+
+router.get('/question', function (req, res, next) {
+	var category = 'world capitals';
+	// var category = req.body.category;
+	// var numOfQuestions = req.body.numOfQuestions;
+	knex('questions').join('category_questions', 'questions.id', 'category_questions.question_id')
+		.join('categories', 'category_questions.category_id', 'categories.id')
+		.select().where('category_name', category)
+	.then(function (data) {
+		res.json(data);
+	})
+})
 
 module.exports = router;
