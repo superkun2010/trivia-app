37da0f36d7caf85f09f1470389609fe2f9ca15af
superkun2010 <kuhsu@hotmail.com>
2016-06-27 15:12:10 -0700

    added game and question model

--- routes/api.js
+++ routes/api.js
@@ -7,14 +7,23 @@ router.get('/', function(req, res, next) {
   res.send('APIs');
 });
 
+router.get('/category', function (req, res, next) {
+	knex('categories').select().then( function (data) {
+		res.json(data);
+	})
+})
+
 router.get('/question', function (req, res, next) {
 	var category = 'world capitals';
+	var numOfQuestions = 1;
 	// var category = req.body.category;
 	// var numOfQuestions = req.body.numOfQuestions;
 	knex('questions').join('category_questions', 'questions.id', 'category_questions.question_id')
 		.join('categories', 'category_questions.category_id', 'categories.id')
-		.select().where('category_name', category)
+		.select().orderBy(knex.raw('RANDOM()')).limit(numOfQuestions)
 	.then(function (data) {
+		console.log(data);
+
 		res.json(data);
 	})
 })
