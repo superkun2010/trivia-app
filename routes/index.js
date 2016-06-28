47c32950affe923136ccf075c4c754d28d3942f6
moniquedesalvo <moniquerenee@gmail.com>
2016-06-28 09:57:02 -0700

    fb login works

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,17 @@ var router = express.Router();
 var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
 var knex = require('../db/knex');
-var passport = require('passport');
-var apiRoutes = require('./api');
 var bcrypt = require('bcrypt');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var passport = require('passport');
+
+var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.use(cookieParser(process.env.SECRET));
 router.use(cookieSession({
@@ -18,6 +26,53 @@ router.use(cookieSession({
     process.env.SESSION_KEY3
   ]
 }));
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/login/facebook/callback",
+    enableProof: true
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      knex('users').where({
+          facebook_oauth: profile.id
+      }).first().then(function(user) {
+        if (!user) {
+          knex('users').insert({ facebook_oauth: profile.id, user_name: profile.displayName, email: 'facebookUser'})
+          .then(function (err, profile) {
+            return cb(null, profile);
+          });
+        } else {
+          return cb(null, profile);
+        }
+     })
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+router.get('/login/facebook', function (req, res, next) {
+    req.logout();
+    res.clearCookie('session');
+    res.clearCookie('session.sig');
+    res.clearCookie('userID');
+    next();
+    },
+   passport.authenticate('facebook')
+);
+
+router.get('/login/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/' }),
+    function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
 
 // Signup/Login with bcrypt
 router.get('/signup', function(req, res, next) {
@@ -30,7 +85,7 @@ router.post('/signup', function(req, res, next) {
     }).first().then(function(user) {
         if (!user) {
             var hash = bcrypt.hashSync(req.body.password, 10);
-            Users().insert({
+            knex('users').insert({
               user_name: req.body.user_name,
               email: req.body.email,
               password: hash
@@ -48,10 +103,14 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
+    req.logout();
+    res.clearCookie('session');
+    res.clearCookie('session.sig');
+    res.clearCookie('userID');
     knex('users').where({
         user_name: req.body.user_name,
     }).first().then(function(user) {
-        if (user && bcrypt.compareSync(req.body.password, user.password) ) {
+        if (user && bcrypt.compareSync(req.body.password, user.password)) {
             res.cookie('userID', user.id, {
                 signed: true
             });
@@ -62,8 +121,14 @@ router.post('/login', function(req, res, next) {
     });
 });
 
+router.get('/', function(req, res, next) {
+    res.render('index', { user: req.session.passport.user.displayName });
+});
+
 router.get('/logout', function(req, res) {
     req.logout();
+    res.clearCookie('session');
+    res.clearCookie('session.sig');
     res.clearCookie('userID');
     res.redirect('/');
 });
