9b769ea30a3cca62227a0dfba6ca42b5cf3e61b3
superkun2010 <kuhsu@hotmail.com>
2016-06-28 11:35:03 -0700

    completed making ajax request for game data

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/mystats', function(req, res, next) {
 	res.render('mystats');
@@ -9,5 +10,10 @@ router.get('/mystats', function(req, res, next) {
 router.get('/start', function(req, res, next) {
   	res.sendfile('public/html/startpage.html');
 });
+
+router.get('/play', function(req, res, next) {
+  	res.sendfile('public/html/gameplay.html');
+});
+
 
 module.exports = router;
