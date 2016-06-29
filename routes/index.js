455c79d1a73cda56e0700ca05c59d94afefdd10b
moniquedesalvo <moniquerenee@gmail.com>
2016-06-28 17:37:23 -0700

    auth routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Main Page');
+  res.render('index');
 });
 
 module.exports = router;
