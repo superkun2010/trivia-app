0207a0eb05381b6714466ae3d3ec1fd6ff1cc384
moniquedesalvo <moniquerenee@gmail.com>
2016-06-28 22:53:10 -0700

    fixed more login issues, formatted login/signup forms, added dynamic features to nav bar

--- routes/auth.js
+++ routes/auth.js
@@ -9,60 +9,8 @@ var bcrypt = require('bcrypt');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var passport = require('passport');
 
-var FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
-var FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-router.use(passport.initialize());
-router.use(passport.session());
-
-router.use(cookieParser(process.env.SECRET));
-router.use(cookieSession({
-  name: 'session',
-  keys: [
-    process.env.SESSION_KEY1,
-    process.env.SESSION_KEY2,
-    process.env.SESSION_KEY3
-  ]
-}));
-
-passport.use(new FacebookStrategy({
-    clientID: FACEBOOK_APP_ID,
-    clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/login/facebook/callback",
-    enableProof: true
-  },
-  function(accessToken, refreshToken, profile, cb) {
-      console.log(profile)
-      knex('users').where({
-          facebook_oauth: profile.id
-      }).first().then(function(user) {
-        if (!user) {
-          knex('users').insert({ facebook_oauth: profile.id, user_name: profile.displayName, email: profile.emails})
-          .then(function (err, profile) {
-            return cb(null, profile);
-          });
-        } else {
-          return cb(null, profile);
-        }
-     })
-  }
-));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
 router.get('/login/facebook', function (req, res, next) {
-    req.logout();
-    res.clearCookie('session');
-    res.clearCookie('session.sig');
-    res.clearCookie('userID');
+    req.session = null;
     next();
     },
    passport.authenticate('facebook', { scope: 'email' })
@@ -86,11 +34,13 @@ router.post('/signup', function(req, res, next) {
     }).first().then(function(user) {
         if (!user) {
             var hash = bcrypt.hashSync(req.body.password, 10);
-            knex('users').insert({
+            knex('users').returning(['id', 'user_name']).insert({
               user_name: req.body.user_name,
               email: req.body.email,
               password: hash
-            }).then(function(){
+          }).then(function(user){
+              req.session.userID = user[0].id;
+              req.session.username = user[0].user_name;
               res.redirect('/game');
             });
         } else {
@@ -104,17 +54,12 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-    req.logout();
-    res.clearCookie('session');
-    res.clearCookie('session.sig');
-    res.clearCookie('userID');
     knex('users').where({
         user_name: req.body.user_name,
     }).first().then(function(user) {
         if (user && bcrypt.compareSync(req.body.password, user.password)) {
             req.session.userID = user.id;
             req.session.username = user.user_name;
-            console.log(req.session);
             res.redirect('/game');
         } else {
             res.redirect('/auth/login');
@@ -125,24 +70,12 @@ router.post('/login', function(req, res, next) {
 
 router.get('/logout', function(req, res) {
     req.session = null;
-    res.redirect('/');
+    res.redirect('/auth/login');
 });
-
-router.use(function (req, res, next) {
-    console.log(req.session)
-    console.log('got here')
-    if (req.session.passport.user.displayName) {
-        req.session.user = req.session.passport.user.displayName
-    }
-    next();
-})
 
 router.get('/', function(req, res, next) {
-    console.log('got here 2')
-    console.log(req)
-    res.render('index', { user: req.session.user});
+    res.render('index', { user: req.session.username});
 });
-
 
 
 module.exports = router;
