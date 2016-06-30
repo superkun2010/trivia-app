19d28f14a92c5c9f67e09e873806b39a4bad497a
moniquedesalvo <moniquerenee@gmail.com>
2016-06-30 09:43:17 -0700

    can add questions

--- public/javascripts/addQuestions.js
+++ public/javascripts/addQuestions.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
     $('select').material_select();
 });
+
+$('#questionType').on('change', function(event) {
+    if (this.selectedIndex === 0) {
+        $('#bool-form').hide();
+        $('#mult-form').hide();
+    }
+    if (this.selectedIndex === 1) {
+        $('#bool-form').show();
+        $('#mult-form').hide();
+    }
+    if (this.selectedIndex === 2) {
+        $('#mult-form').show();
+        $('#bool-form').hide();
+    }
+    event.preventDefault();
+});
